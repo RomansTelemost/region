a4702cb307e485bc67c2392f88f42a832f0873d9
Roman <Leo_dialog@mail.ru>
2024-12-27 21:53:16 +0700

    Handle RegionNotFoundException exception

--- src/main/java/com/dictionary/region/customExceptionAdviceHandler/CustomExceptionHandlerAdvice.java
+++ src/main/java/com/dictionary/region/customExceptionAdviceHandler/CustomExceptionHandlerAdvice.java
@@ -1,5 +1,6 @@
 package com.dictionary.region.customExceptionAdviceHandler;
 
+import com.dictionary.region.exception.RegionNotFoundException;
 import com.dictionary.region.exception.SqlProcessingException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,5 +15,12 @@ public class CustomExceptionHandlerAdvice {
         return new ResponseEntity<>(
                 new AppError(HttpStatus.BAD_REQUEST.value(), e.getMessage()),
                 HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<AppError> catchIllegalArgumentException(RegionNotFoundException e) {
+        return new ResponseEntity<>(
+                new AppError(HttpStatus.NOT_FOUND.value(), e.getMessage()),
+                HttpStatus.NOT_FOUND);
     }
 }
