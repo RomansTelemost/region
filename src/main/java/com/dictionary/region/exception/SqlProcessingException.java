f67214e3f26f98ab32495b5eb5264ccc4b9bfd65
Roman <Leo_dialog@mail.ru>
2025-01-12 16:03:28 +0700

    Spring data jpa

--- src/main/java/com/dictionary/region/exception/SqlProcessingException.java
+++
@@ -1,8 +0,0 @@
-package com.dictionary.region.exception;
-
-public class SqlProcessingException extends RuntimeException {
-
-    public SqlProcessingException(Throwable cause) {
-        super(cause);
-    }
-}
